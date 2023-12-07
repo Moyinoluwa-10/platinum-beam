af4281cd35619e3fc112cf5d1e47f6f62e09e6b0
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-12-07 11:58:34 -0600

    feat: completed homepage responsiveness

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -28,11 +28,11 @@ const Home = () => {
         <Header />
         <div className="p-5 pb-20 pt-[10%]">
           <div className="container">
-            <h1 className="text-5xl max-w-2xl mb-5">
+            <h1 className="text-4xl md:text-5xl max-w-2xl mb-5">
               Step into a World of Immaculate Luxury with Platinum Beam Concept
               Ltd.
             </h1>
-            <p className="text-2xl max-w-2xl mb-9">
+            <p className="text-xl md:text-2xl max-w-2xl mb-9">
               It's not about routines; it's about transforming your space into a
               haven of pristine beauty. Discover a cleaning service that cares
               about the details as much as you do.
@@ -40,7 +40,7 @@ const Home = () => {
             <div>
               <Link
                 to={"/book"}
-                className="uppercase p-7 bg-primary rounded-2xl text-white inline-block"
+                className="uppercase py-6 px-16 md:p-7 bg-primary rounded-2xl text-white inline-block md:text-2xl"
               >
                 Book Now
               </Link>
@@ -49,25 +49,25 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="p-5 py-20 container">
+      <section className="p-5 pt-20 md:pb-20 container">
         <p className="text-center text-secondary text-3xl mb-8">Trusted by</p>
-        <div className="flex max-w-4xl mx-auto justify-between gap-5 flex-wrap items-center">
-          <img src={mtn} alt="mtn-logo" />
-          <img src={piggyvest} alt="piggyvest-logo" />
-          <img src={chipper} alt="chipper-logo" />
-          <img src={dominos} alt="dominos-logo" />
+        <div className="flex max-w-4xl mx-auto justify-evenly gap-x-5 gap-y-7 flex-wrap items-center">
+          <img src={mtn} alt="mtn-logo" className="h-6 md:h-10" />
+          <img src={piggyvest} alt="piggyvest-logo" className="h-6 md:h-10" />
+          <img src={chipper} alt="chipper-logo" className="h-6 md:h-10" />
+          <img src={dominos} alt="dominos-logo" className="h-6 md:h-10" />
         </div>
       </section>
 
-      <section className="p-5 py-24 container flex justify-between items-center gap-12">
+      <section className="p-5 py-24 container flex flex-col lg:flex-row justify-between items-center gap-12">
         <div>
           <img src={professional} alt="image" />
         </div>
-        <div className="max-w-xl">
-          <h3 className="text-secondary text-6xl mb-4 font-bold">
+        <div className="lg:max-w-xl">
+          <h3 className="text-secondary text-4xl md:text-6xl mb-4 font-bold">
             Professional Cleaning Services
           </h3>
-          <p className="text-2xl mb-10">
+          <p className="text-xl md:text-2xl mb-10">
             Get in touch with us today to learn how our team can help you clean
             and de-clutter. Choose from any of our three cleaning plans designed
             to cater to your lifestyle.
@@ -75,7 +75,7 @@ const Home = () => {
           <div>
             <Link
               to={"/book"}
-              className="uppercase p-7 bg-primary rounded-2xl text-white inline-block"
+              className="uppercase py-6 px-14 md:p-7 bg-primary rounded-2xl text-white inline-block"
             >
               Book Now
             </Link>
@@ -83,11 +83,11 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="container p-5 py-36">
-        <h3 className="text-secondary text-6xl mb-6 font-bold text-center">
+      <section className="container p-5 py-5 md:pt-36 pb-36">
+        <h3 className="text-secondary text-4xl md:text-6xl mb-6 font-bold text-center">
           Our Services
         </h3>
-        <p className="text-2xl mb-16 text-center max-w-5xl mx-auto">
+        <p className="text-xl md:text-2xl mb-16 text-center max-w-5xl mx-auto">
           Explore our diverse range of services designed to cater to your
           specific needs. Whether you're a corporate office, an industrial
           giant, or a cozy residence, our skilled team ensures each space is
@@ -96,15 +96,15 @@ const Home = () => {
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-20 justify-between">
           <div>
             <img src={service1} alt="service-image" />
-            <h4 className="text-secondary text-3xl mb-2 font-bold">
+            <h4 className="text-secondary text-2xl md:text-3xl mb-2 font-bold mt-6">
               Corporate Cleaning
             </h4>
-            <p className="text-2xl mb-10 max-w-5xl">
+            <p className="text-xl md:text-2xl mb-5 md:mb-8 max-w-5xl">
               Elevate your workspace with our premium corporate cleaning
               services.
             </p>
             <p>
-              <a href="" className="text-primary text-3xl">
+              <a href="" className="text-primary text-xl md:text-2xl">
                 Let's do this
               </a>
             </p>
@@ -112,14 +112,14 @@ const Home = () => {
 
           <div>
             <img src={service2} alt="service-image" />
-            <h4 className="text-secondary text-3xl mb-2 font-bold">
+            <h4 className="text-secondary text-2xl md:text-3xl mb-2 font-bold mt-6">
               Industrial Maintenance
             </h4>
-            <p className="text-2xl mb-10 max-w-5xl">
+            <p className="text-xl md:text-2xl mb-5 md:mb-8 max-w-5xl">
               We keep your industrial facilities spotless and operational.
             </p>
             <p>
-              <a href="" className="text-primary text-3xl">
+              <a href="" className="text-primary text-xl md:text-2xl">
                 Let's do this
               </a>
             </p>
@@ -127,12 +127,15 @@ const Home = () => {
 
           <div>
             <img src={service3} alt="service-image" />
-            <h4 className="text-secondary text-3xl mb-2 font-bold">
+            <h4 className="text-secondary text-2xl md:text-3xl mb-2 font-bold mt-6">
               Commercial Spaces
             </h4>
-            <p className="text-2xl mb-10 max-w-5xl">Commercial Spaces</p>
+            <p className="text-xl md:text-2xl mb-5 md:mb-8 max-w-5xl">
+              Make a lasting impression with our professional cleaning for
+              commercial areas.
+            </p>
             <p>
-              <a href="" className="text-primary text-3xl">
+              <a href="" className="text-primary text-xl md:text-2xl">
                 Let's do this
               </a>
             </p>
@@ -142,15 +145,15 @@ const Home = () => {
 
           <div>
             <img src={service4} alt="service-image" />
-            <h4 className="text-secondary text-3xl mb-2 font-bold">
+            <h4 className="text-secondary text-2xl md:text-3xl mb-2 font-bold mt-6">
               Residential Bliss
             </h4>
-            <p className="text-2xl mb-10 max-w-5xl">
+            <p className="text-xl md:text-2xl mb-5 md:mb-8 max-w-5xl">
               Experience luxury in the comfort of your home with our residential
               cleaning services.
             </p>
             <p>
-              <a href="" className="text-primary text-3xl">
+              <a href="" className="text-primary text-xl md:text-2xl">
                 Let's do this
               </a>
             </p>
@@ -158,24 +161,24 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="container p-5 py-36">
-        <h3 className="text-secondary text-6xl mb-6 font-bold text-center">
+      <section className="container p-5 py-5 md:pt-36 pb-36">
+        <h3 className="text-secondary text-4xl md:text-6xl mb-6 font-bold text-center">
           Why Choose Us
         </h3>
-        <p className="text-2xl mb-10 text-center max-w-5xl mx-auto">
+        <p className="text-xl md:text-2xl mb-10 text-center max-w-5xl mx-auto">
           Setting the Standard for Excellence
         </p>
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-20 justify-evenly content-evenly items-center justify-items-end">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-x-5 gap-y-5 md:gap-y-20 justify-evenly content-evenly items-center justify-items-end">
           <div className="text-center">
             <img
               src={luxurious}
               alt="cleaning-service-logo"
               className="mx-auto"
             />
-            <h4 className="text-secondary text-3xl my-2 font-bold">
+            <h4 className="text-secondary text-2xl md:text-3xl my-2 font-bold">
               Luxurious Experience
             </h4>
-            <p className="text-2xl mb-10 max-w-5xl">
+            <p className="text-xl md:text-2xl mb-10 max-w-5xl">
               We turn cleaning into a lavish affair, setting new standards in
               the industry.
             </p>
@@ -183,10 +186,10 @@ const Home = () => {
 
           <div className="text-center">
             <img src={attention} alt="details-logo" className="mx-auto" />
-            <h4 className="text-secondary text-3xl my-2 font-bold">
+            <h4 className="text-secondary text-2xl md:text-3xl my-2 font-bold">
               Attention to Detail
             </h4>
-            <p className="text-2xl mb-10 max-w-5xl">
+            <p className="text-xl md:text-2xl mb-10 max-w-5xl">
               Our commitment to detail ensures every nook and cranny is
               spotless.
             </p>
@@ -194,10 +197,10 @@ const Home = () => {
 
           <div className="text-center">
             <img src={proficient} alt="dry-cleaning-logo" className="mx-auto" />
-            <h4 className="text-secondary text-3xl my-2 font-bold">
+            <h4 className="text-secondary text-2xl md:text-3xl my-2 font-bold">
               Proficient Skills
             </h4>
-            <p className="text-2xl mb-10 max-w-5xl">
+            <p className="text-xl md:text-2xl mb-10 max-w-5xl">
               Our skilled team brings expertise and efficiency to every cleaning
               task.
             </p>
@@ -211,10 +214,10 @@ const Home = () => {
               alt="cleaning-bucket-logo"
               className="mx-auto"
             />
-            <h4 className="text-secondary text-3xl my-2 font-bold">
+            <h4 className="text-secondary text-2xl md:text-3xl my-2 font-bold">
               Superior Standards
             </h4>
-            <p className="text-2xl mb-10 max-w-5xl">
+            <p className="text-xl md:text-2xl mb-10 max-w-5xl">
               We pride ourselves on maintaining the highest standards in the
               cleaning industry.
             </p>
@@ -222,15 +225,15 @@ const Home = () => {
         </div>
       </section>
 
-      <section className="p-5 py-24 container flex gap-12 justify-between items-center">
+      <section className="p-5 pt-5 md:pt-24 pb-24 container flex flex-col lg:flex-row gap-12 justify-between items-center">
         <div>
           <img src={transform} alt="image" />
         </div>
-        <div className="max-w-xl">
-          <h3 className="text-secondary text-6xl mb-4 font-bold max-w-md">
+        <div className="lg:max-w-xl">
+          <h3 className="text-secondary text-4xl md:text-6xl mb-4 font-bold lg:max-w-md">
             Let's Transform Your Space
           </h3>
-          <p className="text-2xl mb-10">
+          <p className="text-xl md:text-2xl mb-10">
             Ready to experience the Platinum Beam difference? Contact us today
             for a consultation and let's embark on the journey to elevate your
             space to new heights of cleanliness and luxury.Get in touch with us
@@ -241,7 +244,7 @@ const Home = () => {
           <div>
             <Link
               to={"/book"}
-              className="uppercase p-7 bg-primary rounded-2xl text-white inline-block"
+              className="uppercase py-6 px-14 md:p-7 bg-primary rounded-2xl text-white inline-block"
             >
               Book Now
             </Link>
