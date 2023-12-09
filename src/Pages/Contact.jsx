26c7505658c91dcd7bac11c49a8290eac6749e82
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2023-12-09 03:32:58 -0600

    feat: created contact section skeleton

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,5 +1,23 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="font-lato">
+      <section className="hero min-h-screen text-white flex flex-col">
+        <Header />
+        <div className="p-5 container flex-grow flex place-items-center">
+          <p className="text-2xl max-w-2xl mx-auto text-center">
+            Contact us <br />
+            Get in touch with us if you have any questions, or you just want to
+            chat
+          </p>
+        </div>
+      </section>
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Contact;
