ba74c341b7d9b581f38a2fd14765cc794c45e71e
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-01-25 19:47:16 -0600

    chore: color change

--- src/Components/Home/ChoiceItem.jsx
+++ src/Components/Home/ChoiceItem.jsx
@@ -6,11 +6,11 @@ const ChoiceItem = ({ img, title, description }) => {
     <>
       {title ? (
         <div className="text-center">
-          <div className="flex items-center justify-center bg-secondary w-24 h-24 rounded-full mx-auto">
+          <div className="flex items-center justify-center bg-secondary w-20 md:w-24 h-20 md:h-24 rounded-full mx-auto">
             <img
               src={img}
               alt="cleaning-service-logo"
-              className="mx-auto w-16 h-16"
+              className="mx-auto w-12 md:w-16 h-12 md:h-16"
             />
           </div>
           <h4 className="text-tertiary text-2xl my-2 font-pt-bold">{title}</h4>
