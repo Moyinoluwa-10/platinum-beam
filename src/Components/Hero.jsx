9be858800c8720d77f0a785c988817dc130b4231
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-01-25 23:27:20 -0600

    chore: hero section update

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 // components
 import Header from "./Header";
 
-const Hero = ({ pageTitle, pageText, bgImg }) => {
+const Hero = ({ pageTitle, pageText, bgImg, children }) => {
   return (
     <section
       style={{
@@ -14,8 +14,13 @@ const Hero = ({ pageTitle, pageText, bgImg }) => {
     >
       <Header />
       <div className="p-5 py-24 container text-center">
-        <h2 className="text-3xl md:text-4xl font-medium mb-1">{pageTitle}</h2>
-        <p className="text-2xl">{pageText}</p>
+        {pageTitle && (
+          <h2 className="text-3xl md:text-4xl font-pt-bold mb-1">
+            {pageTitle}
+          </h2>
+        )}
+        {pageText && <p className="text-2xl">{pageText}</p>}
+        {children}
       </div>
     </section>
   );
@@ -25,6 +30,7 @@ Hero.propTypes = {
   bgImg: PropTypes.string,
   pageTitle: PropTypes.string,
   pageText: PropTypes.string,
+  children: PropTypes.node,
 };
 
 export default Hero;
