25042fe7ed81a99dfe0631b5af8168c757ca1f88
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-02-01 08:00:39 -0600

    feat: created config file

---
+++ backend/config/config.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+
+const config = {
+  PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGODB_URI: process.env.MONGODB_URI,
+  NODEMAILER_HOST: process.env.NODEMAILER_HOST,
+  NODEMAILER_PORT: process.env.NODEMAILER_PORT,
+  NODEMAILER_USER: process.env.NODEMAILER_USER,
+  NODEMAILER_PASS: process.env.NODEMAILER_PASS,
+  NODEMAILER_FROM: process.env.NODEMAILER_FROM,
+};
+
+module.exports = config;
