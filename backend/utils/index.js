309c0ec21e9aaacea6ac51b84805b506fbac641a
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-02-01 08:43:44 -0600

    feat: added email utilities

---
+++ backend/utils/index.js
@@ -0,0 +1,11 @@
+const sendBookingClientEmail = require("./sendBookingClientEmail");
+const sendBookingHostEmail = require("./sendBookingHostEmail");
+const sendContactClientEmail = require("./sendContactClientEmail");
+const sendContactHostEmail = require("./sendContactHostEmail");
+
+module.exports = {
+  sendBookingClientEmail,
+  sendBookingHostEmail,
+  sendContactClientEmail,
+  sendContactHostEmail,
+};
