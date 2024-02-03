a3075b9e52d910012b9e780290dc19864cac92ad
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-02-03 12:31:07 -0600

    feat: booking host email temolate

--- backend/utils/sendBookingHostEmail.js
+++ backend/utils/sendBookingHostEmail.js
@@ -8,96 +8,149 @@ const mail = {
 };
 
 const sendBookingClientEmail = async ({ data }) => {
-  mail.html = `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Email Template</title>
-    </head>
-    <body>
+  mail.html = `<body>
+  <div
+    style="
+      background-color: #f2f5f8;
+      padding: 30px 20px;
+      font-family: sans-serif;
+    "
+  >
+    <div style="width: 600px; max-width: 100%; margin: 0 auto">
+      <div>
+        <img
+          src="https://platinum-beam.vercel.app/logo.png"
+          style="width: 80px; height: 80px"
+        />
+      </div>
+
       <div
         style="
-          background-color: #f2f5f8;
-          padding: 30px 20px;
-          font-family: sans-serif;
+          border-radius: 4px;
+          padding: 25px;
+          background: #fff;
+          margin-top: 20px;
         "
       >
-        <div style="width: 600px; max-width: 100%; margin: 0 auto">
-          <div>
-            <img
-              src="https://platinum-beam.vercel.app/logo.png"
-              alt="logo"
-              style="width: 80px; height: 80px"
-            />
-          </div>
-  
-          <div
-            style="
-              border-radius: 4px;
-              padding: 25px;
-              background: #fff;
-              margin-top: 20px;
-            "
-          >
-            <p style="font-size: 16px; color: #333">
-            Hello Platinum Beam, <br />
-            The booking form was just filled with information. Here are the
-            details: <br /><br />
-            <b>Message:</b> ${data} <br /><br />
+        <p style="font-size: 16px; color: #333">
+          Hello Platinum Beam, <br />
+          The booking form was just filled with information. Here are the
+          details: <br /><br />
+          <b>First Name:</b> ${data.firstName} <br />
+          <b>Last Name:</b> ${data.lastName} <br />
+          <b>Phone Number:</b> ${data.phoneNumber} <br />
+          <b>Email Address:</b> ${data.email} <br />
+          <b>House Address:</b> ${data.address} <br />
+          <b>City:</b> ${data.city} <br />
+          <b>State:</b> ${data.state} <br />
+          <b>Cleaning Service:</b> ${data.cleaningService} <br />
+          ${data.name && <>Name: ${data.xyz}</>}
+          ${
+            data.homeType && (
+              <>
+                <b>Home Type:</b> ${data.homeType} <br />
+              </>
+            )
+          }
+          ${
+            data.homeTypeOther && (
+              <>
+                <b>Home Type (Other):</b> ${data.homeTypeOther} <br />
+              </>
+            )
+          }
+          ${
+            data.commercialType && (
+              <>
+                <b>Commercial Type:</b> ${data.commercialType} <br />
+              </>
+            )
+          }
+          ${
+            data.commercialTypeOther && (
+              <>
+                <b>Commercial Type (Other):</b> ${data.commercialTypeOther}{" "}
+                <br />
+              </>
+            )
+          }
+          <b>Cleaning Frequency:</b> ${data.cleaningFrequency} <br />
+          ${
+            data.cleaningMode && (
+              <>
+                <b>Cleaning Mode:</b> ${data.cleaningMode}
+                <br />
+              </>
+            )
+          } 
+          ${
+            data.noOfBathrooms && (
+              <>
+                <b>Number of Bathrooms:</b> ${data.noOfBathrooms} <br />
+              </>
+            )
+          }
+          ${
+            data.noOfBedrooms && (
+              <>
+                <b>Number of Bedrooms:</b> ${data.noOfBedrooms} <br />
+              </>
+            )
+          }
+          ${
+            data.noOfLivingRooms && (
+              <>
+                <b>Number of Living Rooms:</b>${data.noOfLivingRooms} <br />
+              </>
+            )
+          } 
+          <b>Booked Date:</b> ${data.date}<br /><br />
 
-            Please go ahead and contact the person.
-            </p>
-          </div>
-  
-          <div style="margin-top: 40px">
-            <p style="font-size: 14px; color: #333">
-              Block A2, Plot 12B, Fajuyi Estate, Off Ilawe Road, Ado Ekiti
-            </p>
-  
-            <div
-              style="
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                margin-top: 10px;
-              "
-            >
-              <img
-                src="https://platinum-beam.vercel.app/logo.png"
-                alt="logo"
-                style="width: 30px; height: 30px"
-              />
-  
-              <div style="margin-left: auto">
-                <a
-                  href="https://www.instagram.com/platinumbeamconcept"
-                  style="margin-left: 10px"
-                  ><img
-                    src="https://platinum-beam.vercel.app/instagram.png"
-                    alt="instagram-logo"
-                /></a>
-                <a
-                  href="https://www.facebook.com/profile.php?id=100094608620510"
-                  style="margin-left: 10px"
-                  ><img
-                    src="https://platinum-beam.vercel.app/facebook.png"
-                    alt="facebook-logo"
-                /></a>
-                <a
-                  href="https://www.linkedin.com/company/platinum-beam-concept-limited/about/"
-                  style="margin-left: 10px"
-                  ><img
-                    src="https://platinum-beam.vercel.app/linkedIn.png"
-                    alt="linkedIn-logo"
-                /></a>
-              </div>
-            </div>
+          Please go ahead and contact the person.
+        </p>
+      </div>
+
+      <div style="margin-top: 40px">
+        <p style="font-size: 14px; color: #333">
+          Block A2, Plot 12B, Fajuyi Estate, Off Ilawe Road, Ado Ekiti
+        </p>
+
+        <div
+          style="
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-top: 10px;
+          "
+        >
+          <img
+            src="https://platinum-beam.vercel.app/logo.png"
+            alt="logo"
+            style="width: 30px; height: 30px"
+          />
+
+          <div style="margin-left: auto">
+            <a
+              href="https://www.instagram.com/platinumbeamconcept"
+              style="margin-left: 10px"
+              ><img src="https://platinum-beam.vercel.app/instagram.png"
+            /></a>
+            <a
+              href="https://www.facebook.com/profile.php?id=100094608620510"
+              style="margin-left: 10px"
+              ><img src="https://platinum-beam.vercel.app/facebook.png"
+            /></a>
+            <a
+              href="https://www.linkedin.com/company/platinum-beam-concept-limited/about/"
+              style="margin-left: 10px"
+              ><img src="https://platinum-beam.vercel.app/linkedIn.png"
+            /></a>
           </div>
         </div>
       </div>
-    </body>
-  </html>`;
+    </div>
+  </div>
+</body>`;
   return sendEmail(mail);
 };
 
