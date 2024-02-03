9b51ce3f4765c79d3744d35e6786f23893e027c9
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-02-03 09:56:25 -0600

    feat: created terms and conditions page

---
+++ frontend/src/Pages/Terms.jsx
@@ -0,0 +1,146 @@
+// components
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
+const Terms = () => {
+  return (
+    <>
+      <Header />
+      <main className="container mt-20 mb-36">
+        <h1 className="text-4xl text-tertiary font-pt-bold mb-5">
+          Terms and Conditions
+        </h1>
+        <h3 className="text-3xl font-pt-bold mb-5">Your Agreement</h3>
+        <p className="text-xl md:text-2xl">
+          These Terms and Conditions outline the agreement between you (the
+          "Client") and [Platinum Beam] regarding the use of our cleaning
+          services. Please read these terms carefully before booking any
+          services. <br />
+          <br />
+          <br />
+          <span className="font-pt-bold">1. Booking and Services</span> <br />
+          <br />
+          Booking: Services can be booked through our website, phone, or email.
+          A valid email address and phone number are required for all bookings.{" "}
+          <br />
+          <br />
+          Quotes: We offer free quotations based on the information you provide
+          about your cleaning needs. The final quote will be confirmed once we
+          you have spoken to our sales representative and the space has been
+          properly assessed <br />
+          <br />
+          Services Offered: We offer a variety of cleaning services, including:
+          Basic Cleaning, Deep Cleaning, Move-in/Move-out Cleaning, Office
+          Cleaning. <br />
+          <br />
+          Customization: We understand that every client has unique needs. We
+          are happy to customize our services to meet your specific
+          requirements. <br />
+          <br />
+          <br />
+          <span className="font-pt-bold">2. Pricing and Payment</span>
+          <br />
+          <br />
+          Pricing: Our prices are based on the size and condition of your
+          property, the type of cleaning service required, and the number of
+          cleaners needed. <br />
+          <br />
+          Payment: Payment is due at the completion of the service unless other
+          arrangements have been made in advance. We accept cash, debit cards.{" "}
+          <br />
+          <br />
+          Cancellation Policy: We require 24 hours notice for any cancellations.
+          Cancellations made within 24 hours will be subject to a cancellation
+          fee. <br />
+          <br />
+          <br />
+          <span className="font-pt-bold">
+            3. Responsibilities of the Client
+          </span>
+          <br />
+          <br />
+          Preparation: Please ensure your property is free of clutter and pets
+          during the cleaning service. <br />
+          <br />
+          Access: You are responsible for providing the Company with access to
+          your property during the scheduled cleaning time. <br />
+          <br />
+          Safety: Please inform us of any safety hazards or concerns you have
+          about your property. <br />
+          <br />
+          Security: You are responsible for the security of your property and
+          belongings during the cleaning service. <br />
+          <br />
+          <br />
+          <span className="font-pt-bold">
+            4. Responsibilities of the Company
+          </span>
+          <br />
+          <br />
+          Quality: We are committed to providing high-quality cleaning services.
+          If you are not satisfied with the service, please notify us within 24
+          hours and we will re-clean the affected area free of charge. <br />
+          <br />
+          Insurance: We are fully insured for liability and property damage.{" "}
+          <br />
+          <br />
+          Employee Conduct: Our cleaning staff are professional and respectful.
+          We will not tolerate any misconduct or unprofessional behavior from
+          our employees. <br />
+          <br />
+          <br />
+          <span className="font-pt-bold">
+            5. Disclaimer and Limitation of Liability
+          </span>{" "}
+          <br />
+          <br />
+          We are not responsible for any damage caused by pre-existing
+          conditions of your property. We are not responsible for any loss or
+          damage to your personal belongings. Our liability is limited to the
+          cost of the cleaning service. <br />
+          <br />
+          <br />
+          <span className="font-pt-bold">6. Termination</span>
+          <br />
+          <br />
+          We reserve the right to terminate our services at any time for any
+          reason, including but not limited to: Non-payment of feesViolation of
+          these Terms and ConditionsProperty damage or safety hazardsAbusive or
+          disrespectful behavior towards our staff <br />
+          <br />
+          <br />
+          <span className="font-pt-bold">7. Confidentiality</span> <br />
+          <br />
+          The cleaning service will not disclose any confidential information
+          obtained during the course of their work, such as financial records,
+          business plans, or personal information. <br />
+          <br />
+          <br />
+          <span className="font-pt-bold">8. Governing Law </span>
+          <br />
+          <br />
+          These Terms and Conditions shall be governed by and construed in
+          accordance with the laws of Nigeria.
+          <br />
+          <br />
+          <br />
+          <span className="font-pt-bold"> 9. Contact Us</span>
+          <br />
+          <br />
+          If you have any questions about these Terms and Conditions, please
+          contact us at: infoplatinumbeam@gmail.com or 09131704005. Thank you
+          for choosing Platinum Beam <br />
+          <br />
+          Confidentiality: The cleaning service will not disclose any
+          confidential information obtained during the course of their work,
+          such as financial records, business plans, or personal information.{" "}
+          <br />
+          <br />
+        </p>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default Terms;
